e5b3dc780e65458cdb4fc43984cbc13ce5c64919
seraphion <djurdjevicster@gmail.com>
2016-04-14 23:21:18 +0200

    Add option toggle

--- app/routes/polls/poll/index.js
+++ app/routes/polls/poll/index.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    toggleOption(vote, option) {
+      vote.toggleOption(option);
+    }
+  },
+
   model() {
     const poll = this.modelFor("polls.poll");
     return this.get('store').createVote(poll);
